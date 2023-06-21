d1d8e14c288016c47cd90c24a049184bcb766005
Emberwind-Studios <89319126+Emberwind-Studios@users.noreply.github.com>
2023-06-21 18:58:47 -0400

    Added playMountMusic function and set scripts to activate this function on load and aura change.

--- MountMusic.lua
+++ MountMusic.lua
@@ -1,10 +1,77 @@
-local mount_music_settings_frame = CreateFrame("Frame", "MountMusicOptionsPanel", InterfaceOptionsFramePanelContainer)
+--Create initial frame--
+local mountMusicSettingsFrame = CreateFrame("Frame", nil, UIParent)
 
-mount_music_settings_frame.panel = CreateFrame("Frame")
+--Table varible to hold frames containers for the mount textureKits
+local mount_music_frame_containers = {}
 
-mount_music_settings_frame.panel.name = "Mount Music"
+--Create options panel  --
+mountMusicSettingsFrame.panel = CreateFrame("Frame")
+mountMusicSettingsFrame.panel.name = "Mount Music"
 
-local cb = CreateFrame("CheckButton", nil, mount_music_settings_frame.panel, "InterfaceOptionsCheckButtonTemplate")
-cb:SetPoint("TOPLEFT", 20, -20)
+InterfaceOptions_AddCategory(mountMusicSettingsFrame.panel)
 
-InterfaceOptions_AddCategory(mount_music_settings_frame.panel)
+local function playMountMusic(self, event, unit)
+    local numMounts = GetNumCompanions("MOUNT")
+
+    if numMounts > 0 then
+        if IsMounted() then
+            for i = 1, numMounts do
+                local creatureID, creatureName, creatureSpellID, icon, isSummoned, mountType = GetCompanionInfo("MOUNT",
+                    i)
+                if isSummoned and creatureName == "Dreadsteed" then
+                    print(creatureName, icon)
+                    SetCVar("Sound_MusicVolume", 0.3)
+                    PlayMusic("Interface/AddOns/MountMusic/audio-files/rag-boss-fight.mp3")
+                else
+                    if isSummoned and creatureName == "Fiery Warhorse" then
+                        print(creatureName, icon)
+                        SetCVar("Sound_MusicVolume", 0.3)
+                        PlayMusic("Interface/AddOns/MountMusic/audio-files/kharazan-theme.mp3")
+                    end
+                end
+            end
+        else
+            print("Music stopped")
+            StopMusic()
+        end
+    else
+        print("Looks like you don't have any mounts.")
+    end
+end
+
+
+mountMusicSettingsFrame:RegisterUnitEvent("UNIT_AURA", "player")
+mountMusicSettingsFrame:RegisterEvent("ADDON_LOADED")
+mountMusicSettingsFrame:SetScript("OnLoad", playMountMusic)
+mountMusicSettingsFrame:SetScript("OnEvent", playMountMusic)
+
+
+-- local btn = CreateFrame("Button", nil, mount_music_settings_frame.panel, "UIPanelButtonTemplate")
+-- btn:SetPoint("CENTER")
+-- btn:SetSize(80, 20)
+-- btn:SetText("hello")
+
+-- btn:SetMovable(true)
+-- btn:EnableMouse(true)
+-- btn:RegisterForDrag("LeftButton")
+-- btn:SetScript("OnDragStart", btn.StartMoving)
+-- btn:SetScript("OnDragStop", btn.StopMovingOrSizing)
+
+-- btn:SetScript("OnClick", handleClick)
+
+
+-- -- Create a frame to hold the mount icon --
+-- local texture = CreateFrame("Frame", nil, mount_music_settings_frame.panel)
+-- texture:SetPoint("LEFT", 50, 200)
+-- texture:SetSize(80, 20)
+
+-- local newTexture = texture:CreateTexture("mount music top icon", "BACKGROUND")
+
+-- newTexture:SetWidth(200)
+-- newTexture:SetHeight(100)
+-- newTexture:SetPoint("TOPLEFT")
+-- newTexture:SetTexture("Interface\\GLUES\\COMMON\\gamelogo-wrathofthelichking.blp")
+
+
+-- mount_music_settings_frame:SetScript("OnLoad", mount_music_settings_frame.InitFrame)
+-- Create Artwork Layer within the main options panel --
